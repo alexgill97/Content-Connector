35c5f3ad00b325f613ea97c3e10ac81748a6b9ac
Alex <alex.gillespie@live.com>
2022-04-18 21:25:19 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,51 @@
-import Head from 'next/head';
+// import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import HomePhoto from '../public/homepage2.jpeg';
+// import Photo2 from '../public/Photography2.jpeg';
 
-import { auth } from '../firebase/clientApp';
+// import { auth } from '../firebase/clientApp';
 
 export default function Home() {
-  return <div>current user: </div>;
+  return (
+    <div>
+      <main>
+        <div className={`${styles.jumbotron} text-center`}>
+          <h1>Content Connector</h1>
+          <p>Helping Local Freelancers Connect with Local Businesses</p>
+        </div>
+        <div className={`${styles.container}`}>
+          <div className={`${styles.aboutBody}`}>
+            <div className={`col-sm-8`}>
+              <h2>
+                <strong>About Company Page</strong>
+              </h2>
+              <h4>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex
+              </p>
+            </div>
+          </div>
+          <div className={`col-sm-6`}>
+            <Image src={HomePhoto} alt="home-page-photo" />
+          </div>
+        </div>
+        <div className={`${styles.container1} bg`}>
+          <h2>Our Values</h2>
+          <h4>
+            <strong>MISSION:</strong> Our mission lorem ipsum..
+          </h4>
+          <div>
+            <strong>VISION:</strong> Our vision Lorem ipsum..
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
