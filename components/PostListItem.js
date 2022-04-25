df3858faa988650c69c257e2086b0e6840405e1a
Alex <alex.gillespie@live.com>
2022-04-25 18:48:15 -0400

    business profile styling

--- components/PostListItem.js
+++ components/PostListItem.js
@@ -1,36 +1,33 @@
-import Link from 'next/link';
-import styles from '../styles/UserList.module.scss';
-import React from 'react';
-import Delete from './Delete';
-import { deleteDoc } from 'firebase/firestore';
+// import Link from 'next/link';
+// import styles from '../styles/UserList.module.scss';
+// import React from 'react';
+// import Delete from './Delete';
+// import { deleteDoc } from 'firebase/firestore';
 
+// const PostListItem = ({ postTitle, description, userid, address }) => {
 
-const PostListItem = ({ postTitle, description, userid, address }) => {
+//   console.log(userid)
+//   const deleteForm = document.querySelector('.delete')
+//   deleteForm.addEventListener('submit', (e) => {
+//     e.preventDefault()
 
-  console.log(userid)
-  const deleteForm = document.querySelector('.delete')
-  deleteForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+//     const docRef = doc(db, 'posts', deleteForm.userid)
+//     deleteDoc(docRef)
+//     .then(()=>)
+//   })
 
-    const docRef = doc(db, 'posts', deleteForm.userid)
-    deleteDoc(docRef)
-    .then(()=>)
-  })
+//   return (
+//     <div className={`${styles.container}`}>
+//       <div className={`${styles.userListBorder}`}>
+//         <div className={`${styles.username}`}>
+//           {postTitle}
+//         </div>
+//       </div>
+//       <div className={`${styles.description}`}> {description} </div>
+//       <div>{address}</div>
+//       <Delete postTitle={postTitle}/>
+//     </div>
+//   );
+// };
 
-
-
-  return (
-    <div className={`${styles.container}`}>
-      <div className={`${styles.userListBorder}`}>
-        <div className={`${styles.username}`}>
-          {postTitle}
-        </div>
-      </div>
-      <div className={`${styles.description}`}> {description} </div>
-      <div>{address}</div>
-      <Delete postTitle={postTitle}/>
-    </div>
-  );
-};
-
-export default PostListItem;
+// export default PostListItem;
