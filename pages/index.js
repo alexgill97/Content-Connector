9a120f9c5a21a298b5a04ecbb6178374236d05c6
Alex <alex.gillespie@live.com>
2022-04-27 15:48:28 -0400

    changed background color accross app

--- pages/index.js
+++ pages/index.js
@@ -10,68 +10,59 @@ export default function Home() {
   const { currentUser, userData } = useContext(AuthContext);
 
   return (
-    <div>
-      <main className={styles.home_main}>
-        <body className={styles.home_body}>
-          <section className={styles.home_left}>
+    <main className={styles.home_main}>
+      <body className={styles.home_body}>
+        <section className={styles.home_left}>
+          <div className={styles.title}>
+            <h3>
+              Connect Freelancers <br />
+              with
+              <br /> Local Businesses.
+            </h3>
+          </div>
+          <div className={styles.body}>
+            <p>
+              In a World where <em>Content</em> is <strong>King</strong>, Find
+              your <strong>Knights.</strong>
+            </p>
+          </div>
+          {currentUser ? (
             <div>
-              <p>
-                In a World where <em>Content</em> is <strong>King</strong>, Find
-                your <strong>Knights.</strong>
-              </p>
+              <h4>Where Business meets Creativity</h4>
             </div>
-            <div className={styles.title}>
-              <h3>
-                Connect Freelancers <br />
-                with
-                <br /> Local Businesses.
-              </h3>
+          ) : (
+            <div className={`${styles.bothButtons}`}>
+              <Link href="/login">
+                <button className={`${styles.button} ${styles.button_login}`}>
+                  Login
+                </button>
+              </Link>
+              <Link href="/register">
+                <button
+                  className={`${styles.button} ${styles.button_register}`}
+                >
+                  Register
+                </button>
+              </Link>
             </div>
-            <div className={styles.body}>
-              typesetting industry. Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s
-            </div>
-            {currentUser ? (
-              <div>
-                <h4>Where Business meets Creativity</h4>
-              </div>
-            ) : (
-              <div className={`${styles.bothButtons}`}>
-                <Link href="/login">
-                  <button className={`${styles.button} ${styles.button_login}`}>
-                    Login
-                  </button>
-                </Link>
-                <Link href="/register">
-                  <button
-                    className={`${styles.button} ${styles.button_register}`}
-                  >
-                    Register
-                  </button>
-                </Link>
-              </div>
-            )}
-          </section>
-          <section className={styles.home_right}>
-            <div className={styles.image_container}>
-              {/* <div className={styles.float1}>test</div>
+          )}
+        </section>
+        <section className={styles.home_right}>
+          <div className={styles.image_container}>
+            {/* <div className={styles.float1}>test</div>
               <div className={styles.float2}>
                 <div className={styles.float2_container}>
                   <h5>Business Satisfacton</h5>
                   <p>over last 30 days</p>
                 </div>
               </div> */}
-              <img
-                src={'LHL_final_homepage.png'}
-                className={`${styles.photo}`}
-              />
-            </div>
-          </section>
-        </body>
-        {/* <div className={styles.bodyPhoto}>
+            <img src={'LHL_final_homepage.png'} className={`${styles.photo}`} />
+          </div>
+        </section>
+      </body>
+      {/* <div className={styles.bodyPhoto}>
           <img src={'final_bottom.png'} />
         </div> */}
-      </main>
-    </div>
+    </main>
   );
 }
